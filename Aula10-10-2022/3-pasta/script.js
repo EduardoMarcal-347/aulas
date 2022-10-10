28bc32a322d5828e324653a5bc347f9e26559726
EduardoMarcal-347 <edumsfilho347@gmail.com>
2022-10-10 20:25:04 -0300

    COMMIT

---
+++ Aula10-10-2022/3-pasta/script.js
@@ -0,0 +1,15 @@
+function incrementar(){
+    cont = document.getElementById("cont").innerHTML++;
+}
+
+function play(){
+
+    setInterval(incrementar, 1000);
+    
+}
+
+function parar(){
+
+    
+
+}
